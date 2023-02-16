c23a4b5bd029fd46a91a2c8dfcdd82499ab9f98b
lsunkey1997 <lsunkey@ya.ru>
2023-02-16 18:23:44 +0500

    v1 adapt

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
-document.querySelector(".burger-button").addEventListener("click", () => {
-  document.querySelector(".burger-button").classList.toggle("active");
-});
+// document.querySelector(".burger-button").addEventListener("click", () => {
+//   document.querySelector(".burger-button").classList.toggle("active");
+// });
 
 new Swiper(".swiper-usual", {
   pagination: {
@@ -124,13 +124,59 @@ new Swiper(".repairing-slider", {
 });
 
 // Default options
-new Accordion(".accordion-container");
+// new Accordion(".accordion-container");
 
 // User options
-new Accordion(".container-second", {
-  duration: 400,
-  showMultiple: true,
-  onOpen: function (currentElement) {
-    console.log(currentElement);
-  },
+// new Accordion(".container-second", {
+//   duration: 400,
+//   showMultiple: true,
+//   onOpen: function (currentElement) {
+//     console.log(currentElement);
+//   },
+// });
+
+
+var openModalButton = document.getElementById("form1-button");
+var form1Modal = document.getElementById("form1-modal");
+
+openModalButton.addEventListener("click", function() {
+  form1Modal.style.display = "block";
 });
+
+var closeModalButton = form1Modal.querySelector(".close");
+closeModalButton.addEventListener("click", function() {
+  form1Modal.style.display = "none";
+});
+
+var form1 = document.getElementById("form1");
+form1.addEventListener("submit", function(event) {
+  event.preventDefault();
+  // form1Modal.style.display = "none";
+});
+
+// Отправка данных на сервер
+function send(event, php){
+  console.log("Отправка запроса");
+  event.preventDefault ? event.preventDefault() : event.returnValue = false;
+  var req = new XMLHttpRequest();
+  req.open('POST', php, true);
+  req.onload = function() {
+      if (req.status >= 200 && req.status < 400) {
+          json = JSON.parse(this.response); // Ебанный internet explorer 11
+          console.log(json);
+          
+          // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+          if (json.result == "success") {
+              // Если сообщение отправлено
+              alert("Сообщение отправлено");
+          } else {
+              // Если произошла ошибка
+              alert("Ошибка. Сообщение не отправлено");
+          }
+      // Если не удалось связаться с php файлом
+      } else {alert("Ошибка сервера. Номер: "+req.status);}}; 
+  
+  // Если не удалось отправить запрос. Стоит блок на хостинге
+  req.onerror = function() {alert("Ошибка отправки запроса");};
+  req.send(new FormData(event.target));
+  }
---
+++ send-message-to-telegram.php
@@ -0,0 +1,47 @@
+<?php
+$msgs = [];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ 
+    $token = "token";
+    $chat_id = "id";
+ 
+    if (!empty($_POST['phone'])){
+        $bot_url = "https://api.telegram.org/bot{$token}/";
+        $urlForPhoto = $bot_url . "sendPhoto?chat_id=" . $chat_id;
+ 
+        if (isset($_POST['phone'])) {
+          if (!empty($_POST['phone'])){
+            $phone = "Телефон: " . "%2B" . strip_tags($_POST['phone']) . "%0A";
+          }
+        }
+ 
+        if (isset($_POST['theme'])) {
+          if (!empty($_POST['theme'])){
+            $theme = "Тема: " .strip_tags($_POST['theme']);
+          }
+        }
+        // Формируем текст сообщения
+        $txt = $phone . $theme;
+ 
+        $sendTextToTelegram = file_get_contents("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}");
+        if ($output && $sendTextToTelegram) {
+            $msgs['okSend'] = 'Спасибо за отправку вашего сообщения!';
+            echo json_encode($msgs);
+        } elseif ($sendTextToTelegram) {
+            $msgs['okSend'] = 'Спасибо за отправку вашего сообщения!';
+            echo json_encode($msgs);
+          return true;
+        } else {
+            $msgs['err'] = 'Ошибка. Сообщение не отправлено!';
+            echo json_encode($msgs);
+            die('Ошибка. Сообщение не отправлено!');
+        }
+ 
+    } else {
+        $msgs['err'] = 'Ошибка. Вы заполнили не все обязательные поля!';
+        echo json_encode($msgs);;
+    }
+} else {
+  header ("Location: /");
+}
+?>
